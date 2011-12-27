5ca36a7f505f0d552881d11e93de555c319a318a
Troy Straszheim <straszheim@willowgarage.com>
2011-12-27 11:52:21 -0800

    chasing some build bugs

--- scripts/jenkins/create_all.sh
+++ scripts/jenkins/create_all.sh
@@ -1,12 +1,12 @@
-(mkdir export && cd export && wget -qr http://50.28.27.175/repos/building/pool/main -A dsc -nd )
+#!/bin/sh -x
+
+wget -r http://50.28.27.175/repos/building/pool/main -A dsc -nd --directory-prefix=export
 
 for repo in \
     catkin genmsg \
     genpy gencpp gentypelibxml genpybindings \
-    roscpp_core std_msgs \
+    roscpp_core std_msgs
 do
-    ./create_debjobs.py \
-        fuerte \
-        git://github.com/wg-debs/${repo}.git \
+    ./create_debjobs.py fuerte git://github.com/wg-debs/${repo}.git \
         --dscs export --commit
 done
