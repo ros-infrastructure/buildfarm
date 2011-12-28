7137e24f63256bbf2cbfe0b149a06caa02be8b4b
Troy Straszheim <straszheim@willowgarage.com>
2011-12-27 17:36:40 -0800

    don't know if that fix is right but at least things dont error out

--- scripts/jenkins/create_all.sh
+++ scripts/jenkins/create_all.sh
@@ -1,11 +1,16 @@
 #!/bin/sh -x
 
+if [ ! -e ./create_debjobs.py ] ; then
+    echo "Run this script in same dir as create_debjobs.py"
+    exit 1
+fi
+
 wget -r http://50.28.27.175/repos/building/pool/main -A dsc -nd --directory-prefix=export
 
 for repo in \
     catkin genmsg \
     genpy gencpp gentypelibxml genpybindings \
-    roscpp_core std_msgs
+    roscpp_core std_msgs common_msgs
 do
     ./create_debjobs.py fuerte git://github.com/wg-debs/${repo}.git \
         --dscs export --commit
