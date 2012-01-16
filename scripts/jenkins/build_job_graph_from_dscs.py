bc3cefdcbd0c4a30326274152c3b5ae5fbb876ba
Tully Foote <tfoote@willowgarage.com>
2012-01-16 14:40:36 -0800

    fixing else logic

--- scripts/jenkins/build_job_graph_from_dscs.py
+++ scripts/jenkins/build_job_graph_from_dscs.py
@@ -21,14 +21,15 @@ def build_graph(repo, dscs_path = None):
     graph = None
     if dscs_path:
         graph = deb_job_graph(dscs_path)
-    try:
-        print "please be patient downloading all dscs from repo %s"%repo
-        tempdir = tempfile.mkdtemp()
-        cmd = "wget -r %s/pool/main -A dsc -nd --directory-prefix=%s"%(repo, tempdir)
-        subprocess.check_call(cmd.split(), stdout=open("/dev/null", "w"), stderr = subprocess.STDOUT)
-        graph = deb_job_graph(tempdir)
-    finally:
-        shutil.rmtree(tempdir)
+    else:
+        try:
+            print "please be patient downloading all dscs from repo %s"%repo
+            tempdir = tempfile.mkdtemp()
+            cmd = "wget -r %s/pool/main -A dsc -nd --directory-prefix=%s"%(repo, tempdir)
+            subprocess.check_call(cmd.split(), stdout=open("/dev/null", "w"), stderr = subprocess.STDOUT)
+            graph = deb_job_graph(tempdir)
+        finally:
+            shutil.rmtree(tempdir)
 
     return graph
     
