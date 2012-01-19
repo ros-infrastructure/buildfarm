58cf7d80cbce6f25005f69c1dcf5a882871ce975
Tully Foote <tfoote@willowgarage.com>
2012-01-19 15:06:06 -0800

    no more natty

--- scripts/jenkins/create_debjobs.py
+++ scripts/jenkins/create_debjobs.py
@@ -20,7 +20,7 @@ def parse_options():
            help='The ros distro. electric, fuerte, galapagos')
     parser.add_argument('--distros', nargs='+',
            help='A list of debian distros. Default: %(default)s',
-           default=['lucid', 'natty', 'oneiric'])
+           default=['lucid', 'oneiric'])
     parser.add_argument('--commit', dest='commit',
            help='Really?', action='store_true')
     parser.add_argument(dest='release_uri',
