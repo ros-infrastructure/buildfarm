bed2285a530b542622ca8ba1b4d53fa4f9f2f306
Tully Foote <tfoote@willowgarage.com>
2012-06-06 13:54:32 -0700

    more packaging infrastructure

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+#import sys
+#sys.path.insert(0, 'src')
+
+#from buildfarm import __version__
+
+setup(name='buildfarm',
+      version='0.0.1',
+      packages=['buildfarm'],
+      package_dir = {'buildfarm':'src/buildfarm'},
+      scripts = ['scripts/setup_apt_root.py'],
+      package_data = {'buildfarm': ['resources/templates/*']},
+      author = "Tully Foote", 
+      author_email = "tfoote@willowgarage.com",
+      url = "http://www.ros.org/wiki/",
+      download_url = "http://pr.willowgarage.com/downloads/buildfarm/", 
+      keywords = ["ROS"],
+      classifiers = [
+        "Programming Language :: Python", 
+        "License :: OSI Approved :: BSD License" ],
+      description = "ROS package library", 
+      long_description = """\
+A library for interacting with the Catkin buildfarm.
+""",
+      license = "BSD"
+      )
