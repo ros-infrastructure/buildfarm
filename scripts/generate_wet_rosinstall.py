5b9af9bba5145e020e9721e84823dffe05af78ff
Tully Foote <tfoote@willowgarage.com>
2012-06-06 18:59:40 -0700

    generate_wet_rosinstall.py working as script

--- scripts/generate_wet_rosinstall.py
+++ scripts/generate_wet_rosinstall.py
@@ -7,13 +7,14 @@ import yaml
 import os
 import distutils.version
 import urllib2
-import json
 import sys
 
 try:
     import urlparse
 except ImportError:
     import urllib.parse as urlparse #py3k
+
+import buildfarm.rosdistro
 
 
 URL_PROTOTYPE="https://raw.github.com/ros/rosdistro/master/releases/%s.yaml"
@@ -23,45 +24,21 @@ def parse_options():
              description='Create a rosinstall file for the wet rosdistro.')
     parser.add_argument(dest='rosdistro',
            help='The ros distro. electric, fuerte, galapagos')
+    parser.add_argument(dest='distro',
+           help='The ubuntu distro. lucid, oneiric, precise')
     args = parser.parse_args()
     return args
 
 
-def compute_rosinstall_snippet(local_name, gbp_url, version, distro_name):
-
-    if version is None:
-        print ("Error version unset for %s"%local_name)
-        return None
-    config = {}
-    config['local-name'] = local_name
-
-    config['version'] = 'upstream/%s'%version
-    #config['version'] = '%s-%s'%(local_name, version)
-    config['uri'] = gbp_url
-    return {'git': config}
 
 import time
 
-def timed_compute_rosinstall_snippet(local_name, gbp_url, version, distro_name):
-    time.sleep(1.0)
-    
-    return compute_rosinstall_snippet(local_name, gbp_url, version, distro_name)
 
 if __name__ == "__main__":
     args = parse_options()
 
-    print("Fetching " + URL_PROTOTYPE%args.rosdistro)
-    repo_map = yaml.load(urllib2.urlopen(URL_PROTOTYPE%args.rosdistro))
-    if 'release-name' not in repo_map:
-        print("No 'release-name' key in yaml file")
-        sys.exit(1)
-    if repo_map['release-name'] != args.rosdistro:
-        print('release-name mismatch (%s != %s)'%(repo_map['release-name'],args.rosdistro))
-        sys.exit(1)
-    if 'gbp-repos' not in repo_map:
-        print("No 'gbp-repos' key in yaml file")
-        sys.exit(1)
+    rd = buildfarm.rosdistro.Rosdistro(args.rosdistro)
+    
+    rosinstall_data = rd.compute_rosinstall_distro(args.rosdistro, args.distro)
 
-    rosinstall_data = [compute_rosinstall_snippet(r['name'], r['url'], r['version'], args.rosdistro) for r in repo_map['gbp-repos'] if 'url' in r and 'name' in r]
-    rosinstall_data = [x for x in rosinstall_data if x]
     print(yaml.safe_dump(rosinstall_data, default_flow_style=False))
--- src/buildfarm/rosdistro.py
+++ src/buildfarm/rosdistro.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+
+from __future__ import print_function
+
 import yaml, urllib2
 
 URL_PROTOTYPE="https://raw.github.com/ros/rosdistro/master/releases/%s.yaml"
@@ -37,6 +40,38 @@ class Rosdistro:
             self._targets = get_target_distros(self._rosdistro)
         return self._targets
 
+    def get_default_target(self):
+        if self._targets is None:
+            self.get_target_distros()
+        if len(self._targets) == 0:
+            print("Warning no targets defined for distro %s"%self._rosdistro)
+            return None
+        return self._targets[0]
+
+    def get_stack_rosinstall_snippet(self, distro = None):
+        if not distro:
+            distro = self.get_default_target()
+            
+
+    def compute_rosinstall_snippet(self, local_name, gbp_url, version, distro_name):
+
+        if version is None:
+            print ("Error version unset for %s"%local_name)
+            return None
+        config = {}
+        config['local-name'] = local_name
+
+        config['version'] = 'upstream/%s'%version
+        config['version'] = 'debian/ros-%s-%s_%s_%s'%(self._rosdistro, local_name, version, distro_name)
+        #config['version'] = '%s-%s'%(local_name, version)
+        config['uri'] = gbp_url
+        return {'git': config}
+
+
+    def compute_rosinstall_distro(self, rosdistro, distro_name):
+        rosinstall_data = [self.compute_rosinstall_snippet(r['name'], r['url'], r['version'], rosdistro) for r in self.repo_map['gbp-repos'] if 'url' in r and 'name' in r and 'version' in r]
+        return rosinstall_data
+        
 
 
 def get_target_distros(rosdistro):
