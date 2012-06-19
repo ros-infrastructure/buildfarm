8b2978ed40be5c54dd37d021302c43db69541ce8
Tully Foote <tfoote@willowgarage.com>
2012-06-19 15:53:56 -0700

    initial import of rosci source

---
+++ src/buildfarm/rosci_main.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+
+import os
+import sys
+
+from optparse import OptionParser
+
+from . import __version__
+from .jenkins_support import load_server_config_file, get_default_server_config_file, JenkinsConfig_to_handle
+from .job_creator import process_jobs, load_jobs_from_file
+
+NAME='rosci'
+
+def rosci_main():
+    print("starting rosci %s"%__version__)
+    parser = OptionParser(usage="usage: %prog <jobs.yaml> <rosdistro-name>", prog=NAME)
+    parser.add_option("-n", dest="fake", action="store_true", help="Don't actually upload to Jenkins", default=False)
+    options, args = parser.parse_args()
+    if len(args) < 2:
+        parser.error("please specify jobs.yaml file and ROS distribution name (e.g. fuerte)")
+
+    jobs_yaml_path = args[0]
+    if not os.path.isfile(jobs_yaml_path):
+        parser.error("invalid jobs.yaml path: %s"%jobs_yaml_path)
+    rosdistro_name = args[1]
+
+    #TODO: this raises on failure
+    server_config = load_server_config_file(get_default_server_config_file())
+    jenkins_handle = JenkinsConfig_to_handle(server_config)
+    jobs_data = load_jobs_from_file(jobs_yaml_path)
+
+    process_jobs(jobs_data, jenkins_handle, rosdistro_name, options.fake)
+    
---
+++ src/buildfarm/rosdep_support.py
@@ -0,0 +1,72 @@
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2012, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from __future__ import print_function
+
+import os
+import sys
+
+import rospkg.stack
+from rospkg.os_detect import OS_UBUNTU
+from rosdep2.catkin_support import get_apt_installer, get_catkin_view, resolve_for_apt, get_ubuntu_targets
+
+#NOTE: this code is very similar to code in catkin-generate-distribution and rosrelease
+
+def resolve_rosdeps(rosdep_keys, rosdistro_name, os_name, os_platform):
+    """
+    :raises: :exc:`rosdep2.catkin_support.ValidationFailed`
+    :raises: :exc:`KeyError`
+    :raises: :exc:`rosdep2.ResolutionError`
+    """
+    assert os_name == OS_UBUNTU
+    assert os_platform
+    assert type(rosdep_keys) == list
+
+    # use the catkin_support module in rosdep2 as it does the same business
+
+    # apt-install resolves data
+    apt_installer = get_apt_installer()
+    # rosdep view is our view into the rosdep database
+    rosdep_view = get_catkin_view(rosdistro_name, os_name, os_platform)
+
+    # iterate through all our keys to resolve
+    ubuntu_deps = set()
+    for dep in rosdep_keys:
+        resolved = resolve_for_apt(dep, rosdep_view, apt_installer, os_name, os_platform)
+        ubuntu_deps.update(resolved)
+    return list(ubuntu_deps)
+    
+
+def catkin_depends_to_apt(stack_xml_file, rosdistro_name, os_name, os_platform):
+    stack = rospkg.stack.parse_stack_file(stack_xml_file)
+    rosdep_keys = [d.name for d in stack.depends]
+    return resolve_rosdeps(rosdep_keys, rosdistro_name, os_name, os_platform)
