03ae5f5653add8d4e1b1ea0063bf30831027f3e8
Dirk Thomas <dthomas@willowgarage.com>
2012-06-19 17:37:18 -0700

    install build dependencies for ci jobs instead of runtime dependencies

--- src/buildfarm/rosdep_support.py
+++ src/buildfarm/rosdep_support.py
@@ -68,5 +68,5 @@ def resolve_rosdeps(rosdep_keys, rosdistro_name, os_name, os_platform):
 
 def catkin_depends_to_apt(stack_xml_file, rosdistro_name, os_name, os_platform):
     stack = rospkg.stack.parse_stack_file(stack_xml_file)
-    rosdep_keys = [d.name for d in stack.depends]
+    rosdep_keys = [d.name for d in stack.build_depends]
     return resolve_rosdeps(rosdep_keys, rosdistro_name, os_name, os_platform)
