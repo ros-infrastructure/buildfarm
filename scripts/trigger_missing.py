66c67ea0d0e2859a268e9d3ed26382347155aed8
Tully Foote <tfoote@willowgarage.com>
2012-09-05 10:49:10 -0700

    adding triggering mechanism to trigger builds of all missing debs.  This is most of what's needed for #30 What's left to do is to check the versions of the sourcedebs, and make sure that the version in the distro file and the sourcedeb in the repo are the same version.  Right now this script will ignore it, even though it's out of date.

---
+++ scripts/trigger_missing.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import argparse
+import shutil
+import sys
+import tempfile
+import urllib2
+import yaml
+
+import pprint
+
+from buildfarm import dependency_walker, jenkins_support, release_jobs
+
+import rospkg.distro
+
+from buildfarm.rosdistro import debianize_package_name
+
+import buildfarm.repo
+
+
+URL_PROTOTYPE = 'https://raw.github.com/ros/rosdistro/master/releases/%s.yaml'
+
+
+def parse_options():
+    parser = argparse.ArgumentParser(
+             description='Create a set of jenkins jobs '
+             'for source debs and binary debs for a catkin package.')
+    parser.add_argument('--fqdn', dest='fqdn',
+           help='The source repo to push to, fully qualified something...',
+           default='50.28.27.175')
+    parser.add_argument(dest='rosdistro',
+           help='The ros distro. electric, fuerte, groovy')
+    parser.add_argument('--distros', nargs='+',
+           help='A list of debian distros. Default: %(default)s',
+           default=[])
+    parser.add_argument('--commit', dest='commit',
+           help='Really?', action='store_true')
+    return parser.parse_args()
+
+
+def compute_missing(distros, fqdn, rosdistro):
+
+    repo_url = 'http://%s/repos/building' % fqdn
+
+    print('Fetching "%s"' % (URL_PROTOTYPE % rosdistro))
+    repo_map = yaml.load(urllib2.urlopen(URL_PROTOTYPE % rosdistro))
+
+
+    # What ROS distro are we configuring?
+    if 'release-name' not in repo_map:
+        print('No "release-name" key in yaml file')
+        sys.exit(1)
+    if repo_map['release-name'] != args.rosdistro:
+        print('release-name mismatch (%s != %s)' % (repo_map['release-name'], args.rosdistro))
+        sys.exit(1)    
+    if 'repositories' not in repo_map:
+        print('No "repositories" key in yaml file')
+    if 'type' not in repo_map or repo_map['type'] != 'gbp':
+        print('Wrong type value in yaml file')
+        sys.exit(1)
+
+    # Figure out default distros.  Command-line arg takes precedence; if
+    # it's not specified, then read targets.yaml.
+    if distros:
+        default_distros = distros
+    else:
+        print('Fetching "%s"' % (URL_PROTOTYPE % 'targets'))
+        targets_map = yaml.load(urllib2.urlopen(URL_PROTOTYPE % 'targets'))
+        my_targets = [x for x in targets_map if rosdistro in x]
+        if len(my_targets) != 1:
+            print('Must have exactly one entry for rosdistro "%s" in targets.yaml' % rosdistro)
+            sys.exit(1)
+        default_distros = my_targets[0][rosdistro]
+
+    arches = ['amd64', 'i386']
+
+    # We take the intersection of repo-specific targets with default
+    # targets.
+    missing = {}
+    for short_package_name, r in repo_map['repositories'].items():
+        if 'url' not in r:
+            print('"url" key missing for repository "%s"; skipping' % r)
+            continue
+        url = r['url']
+        if 'target' not in r or r['target'] == 'all':
+            target_distros = default_distros
+        else:
+            target_distros = list(set(r['target']) & set(default_distros))
+
+        print ('Configuring WET stack "%s" for "%s"' % (r['url'], target_distros))
+        
+        # todo check if sourcedeb is present with the right version
+        deb_name = debianize_package_name(rosdistro, short_package_name)
+        
+
+        missing = {}
+        for d in target_distros:
+            missing[deb_name] = []
+            if not buildfarm.repo.deb_in_repo(repo_url, deb_name, ".*", d, arch='na', source=True):
+                missing[deb_name].append('source')
+            for a in arches:
+                if not buildfarm.repo.deb_in_repo(repo_url, deb_name, ".*", d, a):
+                    missing[deb_name].append('%s_%s' % (d, a))
+
+                                               
+        # if not trigger sourcedeb
+
+        # else if binaries don't exist trigger them
+        for d in target_distros:
+            for a in arches:
+                pass#missing[short_package_name] = ['source']
+        
+
+
+        
+
+    #dry stacks
+    # dry dependencies
+    dist = rospkg.distro.load_distro(rospkg.distro.distro_uri(rosdistro))
+
+    distro_arches = []
+    for d in default_distros:
+        for a in arches:
+            distro_arches.append( (d, a) )
+
+    for s in dist.stacks:
+        print ("Configuring DRY job [%s]" % s)
+        missing[s] = []
+        # for each distro arch check if the deb is present. If not trigger the build. 
+        for (d, a) in distro_arches:
+            if not buildfarm.repo.deb_in_repo(repo_url, debianize_package_name(rosdistro, s), ".*", d, a):
+                missing[s].append( '%s_%s' % (d, a) )
+
+
+    pp = pprint.PrettyPrinter()
+    print ("net Missing")
+    pp.pprint (missing)
+
+
+    return missing
+
+if __name__ == '__main__':
+    args = parse_options()
+
+
+
+    missing = compute_missing(
+        args.distros,
+        args.fqdn,
+        rosdistro=args.rosdistro)
+
+
+    if args.commit:
+        jenkins_instance = jenkins_support.JenkinsConfig_to_handle(jenkins_support.load_server_config_file(jenkins_support.get_default_catkin_debs_config()))
+
+
+        for s, dist_archs in missing.iteritems():
+            if 'source' in dist_archs:
+                job_name = '%s_sourcedeb' % (debianize_package_name(args.rosdistro, s) )
+                print ("Triggering %s" % (job_name) )
+                try:
+                    jenkins_instance.build_job(job_name)
+                except Exception, ex:
+                    print ("Failed to trigger %s: %s" % (job_name, ex))
+                # don't trigger binaries as the sourcedeb will trigger them anyway
+                continue
+    
+            for da in dist_archs:
+                job_name = '%s_binarydeb_%s' % (debianize_package_name(args.rosdistro, s), da )
+                print ("Triggering %s" % (job_name) )
+                try: 
+                    jenkins_instance.build_job(job_name)
+                except Exception, ex:
+                    print ("Failed to trigger %s: %s" % (job_name, ex) )
+
+
+    else:
+        
+        print('This was not pushed to the server.  If you want to do so use "--commit" to do it for real.')
---
+++ src/buildfarm/repo.py
@@ -0,0 +1,203 @@
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2010, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Revision $Id: repo.py 16975 2012-09-03 05:02:44Z tfoote $
+
+"""
+Utilities for reading state from a debian repo
+"""
+
+import urllib
+import urllib2
+import re
+import os 
+import tempfile
+import shutil
+import gzip
+
+#from .core import debianize_name
+
+class BadRepo(Exception): pass
+
+_Packages_cache = {}
+def get_Packages(repo_url, os_platform, arch, cache=None):
+    """
+    Retrieve the package list from the shadow repo. This routine
+    utilizes a cache and should not be invoked in long-running
+    processes.
+    @raise BadRepo: if repo does not exist
+    """
+    if cache is None:
+        cache = _Packages_cache
+
+    # this is very bad.  This script is assuming the layout of the
+    # repo has a subdirectory ubuntu.  I can't parameterize it out
+    # without potentially breaking a lot. Using an if statement to get
+    # it to work.
+    if 'packages.ros.org/ros' in repo_url or 'shadow' in repo_url:
+        packages_url = repo_url + '/ubuntu/dists/%(os_platform)s/main/binary-%(arch)s/Packages'%locals()
+    else:
+        packages_url = repo_url + '/dists/%(os_platform)s/main/binary-%(arch)s/Packages'%locals()
+    if packages_url in cache:
+        return cache[packages_url]
+    else:
+        try:
+            cache[packages_url] = retval = urllib2.urlopen(packages_url).read()
+        except urllib2.HTTPError:
+            raise BadRepo("[%s]: %s"%(repo_url, packages_url))
+    return retval
+    
+def get_source_Packages(repo_url, os_platform, cache=None):
+    """
+    Retrieve the package list from the shadow repo. This routine
+    utilizes a cache and should not be invoked in long-running
+    processes.
+    @raise BadRepo: if repo does not exist
+    """
+    if cache is None:
+        cache = _Packages_cache
+
+    # this is very bad.  This script is assuming the layout of the
+    # repo has a subdirectory ubuntu.  I can't parameterize it out
+    # without potentially breaking a lot. Using an if statement to get
+    # it to work.
+    if 'packages.ros.org/ros' in repo_url or 'shadow' in repo_url:
+        packages_url = repo_url + '/ubuntu/dists/%(os_platform)s/main/source/Sources.gz'%locals()
+    else:
+        packages_url = repo_url + '/dists/%(os_platform)s/main/source/Sources.gz'%locals()
+    if packages_url in cache:
+        return cache[packages_url]
+    else:
+        d = tempfile.mkdtemp()
+        try:
+            tar_f = urllib.urlretrieve(packages_url, os.path.join(d, 'Source.gz'))
+            tar_h = gzip.open(tar_f[0],'r:gz')
+            cache[packages_url] = retval = tar_h.read()
+        except urllib2.HTTPError:
+            raise BadRepo("[%s]: %s"%(repo_url, packages_url))
+        finally:
+            if os.path.isdir(d):
+                shutil.rmtree(d)
+    return retval
+
+
+def parse_Packages(packagelist):
+    """
+    Parse debian Packages list into (package, version, depends) tuples
+    @return: parsed tuples or None if packagelist is None
+    """
+    package_deps = []
+    package = deps = version = distro = None
+    for l in packagelist.split('\n'):
+        if l.startswith('Package: '):
+            package = l[len('Package: '):]
+        elif l.startswith('Version: '):
+            version = l[len('Version: '):]
+        elif l.startswith('Depends: '):
+            deps = l[len('Depends: '):].split(',')
+            deps = [d.strip() for d in deps]
+        elif l.lower().startswith('wg-rosdistro: '):
+            distro = l[len('wg-rosdistro: '):]
+        if package != None and version != None and deps != None and distro != None:
+            package_deps.append((package, version, deps, distro))
+            package = version = deps = distro = None
+    return package_deps
+    
+def load_Packages(repo_url, os_platform, arch, cache=None, source=False):
+    """
+    Download and parse debian Packages list into (package, version, depends) tuples
+    """
+    if source:
+        return parse_Packages(get_source_Packages(repo_url, os_platform, cache))
+    return parse_Packages(get_Packages(repo_url, os_platform, arch, cache))
+
+def get_repo_version(repo_url, distro, os_platform, arch, source=False):
+    """
+    Return the greatest build-stamp for any deb in the repository
+    """
+    packagelist = load_Packages(repo_url, os_platform, arch, source)
+    return max(['0'] + [x[1][x[1].find('-')+1:x[1].find('~')] for x in packagelist if x[3] == distro.release_name])
+
+
+def deb_in_repo(repo_url, deb_name, deb_version, os_platform, arch, use_regex=True, cache=None, source=False):
+    """
+    @param cache: dictionary to store Packages list for caching
+    """
+    if source:
+        packagelist = get_source_Packages(repo_url, os_platform, cache)
+    else:
+        packagelist = get_Packages(repo_url, os_platform, arch, cache)
+    if not use_regex:
+        s = 'Package: %s\nVersion: %s'%(deb_name, deb_version)
+        return s in packagelist
+    else:
+        M = re.search('^Package: %s\nVersion: %s$'%(deb_name, deb_version), packagelist, re.MULTILINE)
+        return M is not None
+
+def get_depends(repo_url, deb_name, os_platform, arch):
+    """
+    Get all debian package dependencies by scraping the Packages
+    list. We mainly use this for invalidation logic. 
+    """
+    # There is probably something much simpler we could do, but this
+    # more robust to any bad state we may have caused to the shadow
+    # repo.
+    package_deps = load_Packages(repo_url, os_platform, arch)
+    done = False
+    queue = [deb_name]
+    depends = set()
+    # This is not particularly efficient, but it does not need to
+    # be. Basically, we find all the packages that depend on the
+    # package, then find all the packages that depends on those,
+    # etc...
+    while queue:
+        next  = queue[0]
+        queue = queue[1:]
+        for package, _, deps, _ in package_deps:
+            #strip of version specifications from deps
+            deps = [d.split()[0] for d in deps]
+            if package not in depends and next in deps:
+                queue.append(package)
+                depends.add(package)
+    return list(depends)
+
+#def get_stack_version(packageslist, distro_name, stack_name):
+#    """
+#    Get the ROS version number of the stack in the repository
+#    """
+#    deb_name = "ros-%s-%s"%(distro_name, debianize_name(stack_name))
+#    match = [vm for sm, vm, _, _ in packageslist if sm == deb_name]
+#    if match:
+#        return match[0].split('-')[0]
+#    else:
+#        return None
+
