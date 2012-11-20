de48712b8d7dc7d41209143463eca24729a48d13
Issac Trotts <itrotts@willowgarage.com>
2012-11-20 15:15:22 -0800

    Add "wet" column. Fix dimension issue in table creation.

--- buildfarm/status_page.py
+++ buildfarm/status_page.py
@@ -4,35 +4,41 @@ import tempfile
 import urllib2
 import yaml
 
+import apt
 import numpy as np
 
 import buildfarm.apt_root
 import buildfarm.rosdistro
 from rospkg.distro import distro_uri
 
-def make_status_page():
+def make_status_page(distro_arches):
     '''
     Returns the contents of an HTML page showing the current
     build status for all wet and dry packages on all
     supported distributions and architectures.
+
+    :param distro_arches: [(distro, arch), ...] from get_distro_arches()
     '''
     # Load lists of wet and dry ROS package names
     wet_names_versions = get_wet_names_versions()
     dry_names_versions = get_dry_names_versions()
 
     # Get the lastest deb version for each package.
-    distros = buildfarm.rosdistro.get_target_distros('groovy')
-    arches = ['source', 'amd64', 'i386']
-    distro_arches = [(d, a) for d in distros for a in arches]
     da_to_pkgs = load_deb_info(distro_arches)
 
     # Make in-memory table showing the latest deb version for each package.
     t = make_versions_table(wet_names_versions, dry_names_versions, da_to_pkgs,
                             distro_arches)
+    logging.info('Table:\n%s', t)
 
     # Generate HTML from the in-memory table
     table_html = make_html_from_table(t)
     return make_html_doc(title='Build status page', body=table_html)
+
+def get_distro_arches():
+    distros = buildfarm.rosdistro.get_target_distros('groovy')
+    arches = ['amd64', 'i386', 'source']
+    return [(d, a) for d in distros for a in arches]
 
 def make_versions_table(wet_names_versions, dry_names_versions, da_to_pkgs,
                         distro_arches):
@@ -41,27 +47,32 @@ def make_versions_table(wet_names_versions, dry_names_versions, da_to_pkgs,
     ros package names and versions followed by debian versions for each
     distro/arch.
     '''
+    ros_pkgs = get_ros_pkgs_table(wet_names_versions, dry_names_versions)
+    left_columns = [('name', object), ('version', object), ('wet', bool)]
     da_strs = ['%s_%s' % (d, a) for d, a in distro_arches]
-    ros_pkgs = np.array(
-        [(name, version, True) for name, version in wet_names_versions] + 
-        [(name, version, False) for name, version in dry_names_versions],
-        dtype=[('name', object), ('version', object), ('wet', bool)])
-
-    left_columns = [('name', object), ('source_version', object)]
     right_columns = [(da_str, object) for da_str in da_strs]
     columns = left_columns + right_columns
-    table = np.empty((len(ros_pkgs), len(columns)), dtype=columns)
-    da_name_to_deb_version = dict(((da_str, p.name), p.version) for da_str, p in
-                              da_to_pkgs.items())
+    table = np.empty(len(ros_pkgs), dtype=columns)
+    da_name_to_deb_version = dict(((da_str, p['name']), p['version'])
+                                  for da_str, pkgs in da_to_pkgs.items()
+                                  for p in pkgs)
 
     for i, (name, version, wet) in enumerate(ros_pkgs):
         table['name'][i] = name
         table['version'][i] = version
+        table['wet'][i] = wet
         for da_str in da_strs:
             debname = buildfarm.rosdistro.debianize_package_name('groovy', name)
-            table[da_str] = da_name_to_deb_version[(da_str, debname)]
+            version = da_name_to_deb_version.get((da_str, debname))
+            table[da_str][i] = version
 
     return table
+
+def get_ros_pkgs_table(wet_names_versions, dry_names_versions):
+    return np.array(
+        [(name, version, True) for name, version in wet_names_versions] + 
+        [(name, version, False) for name, version in dry_names_versions],
+        dtype=[('name', object), ('version', object), ('wet', bool)])
 
 def make_html_from_table(table):
     '''
@@ -85,8 +96,10 @@ def load_deb_info(distro_arches):
         buildfarm.apt_root.setup_apt_rootdir(da_rootdir, distro, arch,
                                              additional_repos=ros_repos)
         logging.info('Getting a list of packages for %s-%s', distro, arch)
-        pkgs = list_packages(da_rootdir, substring='ros-groovy')
-        logging.info('Result: %s', pkgs)
+        cache = apt.Cache(rootdir=rootdir)
+        cache.open()
+        pkgs = [nv for nv in get_names_versions_from_apt_cache(cache)
+                if 'ros-groovy' in nv['name']]
         packages[dist_arch] = pkgs
     return packages
 
@@ -169,14 +182,8 @@ def make_html_table(header, rows):
 </table>
 ''' % (header_str, rows_str)
 
-def list_packages(rootdir, substring):
-    import apt
-    c = apt.Cache(rootdir=rootdir)
-    c.open()
-    c.update()
-    c.open() # This is required after updating to avoid querying the old data.
-    return [{'name': k, 'version': c[k].candidate.version}
-            for k in c.keys() if substring in k]
+def get_names_versions_from_apt_cache(cache):
+    return [{'name': k, 'version': cache[k].candidate.version} for k in cache.keys()]
 
 def main():
     import BaseHTTPServer
@@ -188,7 +195,8 @@ def main():
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            page = make_status_page()
+            distro_arches = get_distro_arches()
+            page = make_status_page(distro_arches)
             self.wfile.write(page)
 
     daemon = BaseHTTPServer.HTTPServer(('', 8080), Handler)
