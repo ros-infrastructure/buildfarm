1a453452da611ff803499a12c472af4b14a9b745
Issac Trotts <itrotts@willowgarage.com>
2012-11-20 14:06:59 -0800

    Add numpy table. Get version info from deb repos.

--- buildfarm/status_page.py
+++ buildfarm/status_page.py
@@ -1,13 +1,14 @@
-import textwrap
+import os
+import logging
+import tempfile
 import urllib2
 import yaml
 
-import rosdistro
+import numpy as np
+
+import buildfarm.apt_root
+import buildfarm.rosdistro
 from rospkg.distro import distro_uri
-
-__all__ = ['make_status_page']
-
-arches = ['amd64', 'i386']
 
 def make_status_page():
     '''
@@ -16,32 +17,96 @@ def make_status_page():
     supported distributions and architectures.
     '''
     # Load lists of wet and dry ROS package names
-    wet_names_pkgs = get_wet_packages()
-    dry_names_pkgs = get_dry_packages()
+    wet_names_versions = get_wet_names_versions()
+    dry_names_versions = get_dry_names_versions()
 
-    # Load build statuses of packages on each distro/arch
+    # Get the lastest deb version for each package.
+    distros = buildfarm.rosdistro.get_target_distros('groovy')
+    arches = ['source', 'amd64', 'i386']
+    distro_arches = [(d, a) for d in distros for a in arches]
+    da_to_pkgs = load_deb_info(distro_arches)
 
-    # Generate an in-memory table with the info to display
+    # Make in-memory table showing the latest deb version for each package.
+    t = make_versions_table(wet_names_versions, dry_names_versions, da_to_pkgs,
+                            distro_arches)
 
     # Generate HTML from the in-memory table
-    header = ['package', 'version']
-    wet_html = make_html_table_from_names_pkgs(header, wet_names_pkgs)
-    dry_html = make_html_table_from_names_pkgs(header, dry_names_pkgs)
-    body = '''
-        <h2>Wet Packages</h2>
-        %s
-        <h2>Dry Packages</h2>
-        %s
-    ''' % (wet_html, dry_html)
-    return make_html_doc(title='Build status page', body=body)
+    table_html = make_html_from_table(t)
+    return make_html_doc(title='Build status page', body=table_html)
 
-def make_html_table_from_names_pkgs(header, names_pkgs):
-    debify = lambda name: rosdistro.debianize_package_name('groovy', name)
+def make_versions_table(wet_names_versions, dry_names_versions, da_to_pkgs,
+                        distro_arches):
+    '''
+    Returns an in-memory table with all the information that will be displayed:
+    ros package names and versions followed by debian versions for each
+    distro/arch.
+    '''
+    da_strs = ['%s_%s' % (d, a) for d, a in distro_arches]
+    ros_pkgs = np.array(
+        [(name, version, True) for name, version in wet_names_versions] + 
+        [(name, version, False) for name, version in dry_names_versions],
+        dtype=[('name', object), ('version', object), ('wet', bool)])
+
+    left_columns = [('name', object), ('source_version', object)]
+    right_columns = [(da_str, object) for da_str in da_strs]
+    columns = left_columns + right_columns
+    table = np.empty((len(ros_pkgs), len(columns)), dtype=columns)
+    da_name_to_deb_version = dict(((da_str, p.name), p.version) for da_str, p in
+                              da_to_pkgs.items())
+
+    for i, (name, version, wet) in enumerate(ros_pkgs):
+        table['name'][i] = name
+        table['version'][i] = version
+        for da_str in da_strs:
+            debname = buildfarm.rosdistro.debianize_package_name('groovy', name)
+            table[da_str] = da_name_to_deb_version[(da_str, debname)]
+
+    return table
+
+def make_html_from_table(table):
+    '''
+    Makes an HTML table from a numpy array with named columns
+    '''
+    header = table.dtype.names
+    rows = [row for row in table]
+    return make_html_table(header, rows)
+
+def load_deb_info(distro_arches):
+    # FIXME: Process each repo separately
+    ros_repos = {'shadow': 'http://packages.ros.org/ros/ubuntu/',
+                 'shadow-fixed': 'http://packages.ros.org/ros-shadow-fixed/ubuntu/',
+                 'ros': 'http://50.28.27.175/repos/building'}
+    rootdir = tempfile.mkdtemp()
+    packages = {}
+    for distro, arch in distro_arches:
+        dist_arch = "%s_%s" % (distro, arch)
+        da_rootdir = os.path.join(rootdir, dist_arch)
+        logging.info('Setting up an apt root directory at %s', da_rootdir)
+        buildfarm.apt_root.setup_apt_rootdir(da_rootdir, distro, arch,
+                                             additional_repos=ros_repos)
+        logging.info('Getting a list of packages for %s-%s', distro, arch)
+        pkgs = list_packages(da_rootdir, substring='ros-groovy')
+        logging.info('Result: %s', pkgs)
+        packages[dist_arch] = pkgs
+    return packages
+
+def make_html_table_from_names_versions(names_pkgs):
+    header = ['package', 'version']
+    debify = lambda name: buildfarm.rosdistro.debianize_package_name('groovy', name)
     rows = [(debify(name), d.get('version')) for name, d in names_pkgs]
     rows.sort(key=lambda (pkg, version): pkg)
     return make_html_table(header, rows)
 
-def get_wet_packages():
+def get_wet_names_versions():
+    return get_names_versions(get_wet_names_packages())
+
+def get_dry_names_versions():
+    return get_names_versions(get_dry_names_packages())
+
+def get_names_versions(names_pkgs):
+    return [(name, d.get('version')) for name, d in names_pkgs]
+
+def get_wet_names_packages():
     '''
     Fetches a yaml file from the web and returns a list of pairs of the form
 
@@ -56,7 +121,7 @@ def get_wet_yaml():
     url = 'https://raw.github.com/ros/rosdistro/master/releases/groovy.yaml'
     return yaml.load(urllib2.urlopen(url))
 
-def get_dry_packages():
+def get_dry_names_packages():
     '''
     Fetches a yaml file from the web and returns a list of pairs of the form
 
@@ -104,8 +169,19 @@ def make_html_table(header, rows):
 </table>
 ''' % (header_str, rows_str)
 
+def list_packages(rootdir, substring):
+    import apt
+    c = apt.Cache(rootdir=rootdir)
+    c.open()
+    c.update()
+    c.open() # This is required after updating to avoid querying the old data.
+    return [{'name': k, 'version': c[k].candidate.version}
+            for k in c.keys() if substring in k]
+
 def main():
     import BaseHTTPServer
+
+    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
     class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
         def do_GET(self):
