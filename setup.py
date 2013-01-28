7f19aaab9bf27ffdfc9122320df085bf676ed2bc
Dirk Thomas <dthomas@willowgarage.com>
2013-01-28 15:01:12 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -10,30 +10,34 @@ try:
 except ImportError:
     pass
 
-setup(name='buildfarm',
-      version='0.0.1',
-      packages=['buildfarm'],
-      package_dir = {'buildfarm':'buildfarm'},
-      scripts = ['scripts/setup_apt_root.py',
-                 'scripts/rosci', 
-                 'scripts/rosci-catkin-depends', 
-                 'scripts/rosci-clean-junit-xml', 
-                 'scripts/build_status_csv_to_html.py'],
-      package_data = {'buildfarm': ['resources/templates/*.em', 'resources/templates/*/*']},
-      install_requires = ['argparse', 'distribute', 'EmPy', 'PrettyTable', 'vcstools', 'rospkg'],
-      test_requires = ['nose'],
-      test_suite = 'nose.collector',
-      author = "Tully Foote", 
-      author_email = "tfoote@willowgarage.com",
-      url = "http://www.ros.org/wiki/",
-      download_url = "http://pr.willowgarage.com/downloads/buildfarm/", 
-      keywords = ["ROS"],
-      classifiers = [
-        "Programming Language :: Python", 
-        "License :: OSI Approved :: BSD License" ],
-      description = "ROS package library", 
-      long_description = """\
-A library for interacting with the Catkin buildfarm.
-""",
-      license = "BSD"
-      )
+setup(
+    name='buildfarm',
+    version='0.0.1',
+    packages=['buildfarm'],
+    package_dir={'buildfarm': 'buildfarm'},
+    scripts=[
+        'scripts/assert_package_dependencies_present.py',
+        'scripts/assert_package_present.py',
+        'scripts/count_ros_packages.py',
+        'scripts/create_release_jobs.py',
+        'scripts/create_release_jobs_fuerte.py',
+        'scripts/generate_sourcedeb',
+        'scripts/generate_status_page.py',
+        'scripts/setup_apt_root.py',
+        'scripts/trigger_missing.py'],
+    package_data={'buildfarm': ['resources/templates/*.em', 'resources/templates/*/*']},
+    install_requires=['argparse', 'catkin_pkg', 'EmPy', 'rospkg', 'vcstools'],
+    author='Tully Foote',
+    author_email='tfoote@willowgarage.com',
+    url='http://www.ros.org/wiki/',
+    download_url='http://pr.willowgarage.com/downloads/buildfarm/',
+    keywords=['ROS'],
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License'],
+    description='ROS package library',
+    long_description='''\
+A library for interacting with the catkin buildfarm.
+''',
+    license='BSD'
+)
