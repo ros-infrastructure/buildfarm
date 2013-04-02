c6cb9b4ba84ee45f54589616c67f552a312e32c4
Tully Foote <tfoote@willowgarage.com>
2013-04-02 15:52:58 -0700

    pep8 pyflake cleanup

--- scripts/generate_status_page.py
+++ scripts/generate_status_page.py
@@ -7,20 +7,25 @@ import os
 import sys
 import time
 
-from buildfarm.status_page import build_repo_caches, get_distro_arches, render_csv, transform_csv_to_html
+from buildfarm.status_page import build_repo_caches, \
+    get_distro_arches, render_csv, transform_csv_to_html
 import buildfarm.status_page
 
 
 def parse_options(args=sys.argv[1:]):
     p = argparse.ArgumentParser(description='Generate the HTML page showing the package build status.')
-    p.add_argument('--basedir', default='/tmp/build_status_page', help='Root directory containing ROS apt caches. This should be created using the build_caches command.')
-    p.add_argument('--skip-fetch', action='store_true', help='Skip fetching the apt data.')
-    p.add_argument('--skip-csv', action='store_true', help='Skip generating .csv file.')
-    p.add_argument('rosdistro', default='groovy', help='The ROS distro to generate the status page for (i.e. groovy).')
-    p.add_argument('--build-repo', 
+    p.add_argument('--basedir', default='/tmp/build_status_page',
+                   help='Root directory containing ROS apt caches. This should be created using the build_caches command.')
+    p.add_argument('--skip-fetch', action='store_true',
+                   help='Skip fetching the apt data.')
+    p.add_argument('--skip-csv', action='store_true',
+                   help='Skip generating .csv file.')
+    p.add_argument('rosdistro', default='groovy',
+                   help='The ROS distro to generate the status page for (i.e. groovy).')
+    p.add_argument('--build-repo',
           default='http://50.28.27.175/repos/building',
           help='Repository URL for the build farm repository.')
-    p.add_argument('--shadow-repo', 
+    p.add_argument('--shadow-repo',
           default='http://packages.ros.org/ros-shadow-fixed/ubuntu/',
           help='Repository URL for the staging repository.')
     p.add_argument('--public-repo',
@@ -50,9 +55,9 @@ if __name__ == '__main__':
 
     distro_arches = []
     if args.da:
-        distro_arches = [ tuple(a.split(',')) for a in args.da ]
+        distro_arches = [tuple(a.split(',')) for a in args.da]
     elif args.distros:
-        distro_arches = [ (d, a) for d in args.distros for a in args.arches ]
+        distro_arches = [(d, a) for d in args.distros for a in args.arches]
     else:
         distro_arches = get_distro_arches(args.arches, args.rosdistro)
 
@@ -65,9 +70,11 @@ if __name__ == '__main__':
     csv_file = os.path.join(args.basedir, '%s.csv' % args.rosdistro)
     if not args.skip_csv:
         print('Generating .csv file...')
-        render_csv(args.basedir, csv_file, args.rosdistro, distro_arches, ros_repos)
+        render_csv(args.basedir, csv_file, args.rosdistro,
+                   distro_arches, ros_repos)
     elif not os.path.exists(csv_file):
-        print('.csv file "%s" is missing. Call script without "--skip-csv".' % csv_file, file=sys.stderr)
+        print('.csv file "%s" is missing. Call script without "--skip-csv".' %\
+                  csv_file, file=sys.stderr)
     else:
         print('Skip generating .csv file')
 
@@ -85,13 +92,14 @@ if __name__ == '__main__':
             view_name = '{rosdistro_short}src'
         else:
             data['arch_short'] = {  'amd64': '64',
-                                    'i386': '32', 
+                                    'i386': '32',
                                     'armel': 'armel',
                                     'armhf': 'armhf'}[jobtype]
             column_label = '{rosdistro_short}bin{distro_short}{arch_short}'
             view_name = '{rosdistro_short}bin{distro_short}{arch_short}'
         data['column_label'] = column_label.format(**data)
-        data['view_url'] = 'http://jenkins.willowgarage.com:8080/view/%s/' % view_name.format(**data)
+        data['view_url'] = 'http://jenkins.willowgarage.com:8080/view/%s/' % \
+            view_name.format(**data)
 
         if is_source:
             job_name = 'ros-{rosdistro}-{{pkg}}_sourcedeb'
@@ -104,7 +112,8 @@ if __name__ == '__main__':
 
     print('Transforming .csv into .html file...')
     with open(csv_file, 'r') as f:
-        html = transform_csv_to_html(f, metadata_builder, args.rosdistro, start_time)
+        html = transform_csv_to_html(f, metadata_builder,
+                                     args.rosdistro, start_time)
     html_file = os.path.join(args.basedir, '%s.html' % args.rosdistro)
     with open(html_file, 'w') as f:
         f.write(html)
@@ -112,7 +121,8 @@ if __name__ == '__main__':
     print('Symlinking jQuery resources...')
     dst = os.path.join(args.basedir, 'jquery')
     if not os.path.exists(dst):
-        src = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'resources', 'jquery')
+        src = os.path.join(os.path.dirname(os.path.dirname(__file__)),
+                           'resources', 'jquery')
         os.symlink(src, dst)
 
     print('Generated .html file "%s"' % html_file)
