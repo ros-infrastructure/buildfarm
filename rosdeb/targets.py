04f743431c73c8a60a1ca9cc4a0ca24477da2a7a
Tully Foote <tfoote@willowgarage.com>
2013-04-16 21:33:25 -0700

    adding hydro as a target for dry

--- rosdeb/targets.py
+++ rosdeb/targets.py
@@ -1,21 +1,23 @@
 os_platform = {
     'testing': ['lucid', 'maverick'],
     'unstable': ['lucid', 'oneiric'],
+    'hydro': ['precise', 'quantal', 'raring'],
     'groovy': ['oneiric', 'precise', 'quantal'],
     'fuerte': ['lucid', 'oneiric', 'precise'],
-    'electric': ['lucid', 'maverick', 'natty', 'oneiric'],
-    'diamondback': ['lucid', 'maverick', 'natty'],
-    'cturtle': ['lucid', 'maverick', 'karmic'],
 # no longer supported
+#    'electric': ['lucid', 'maverick', 'natty', 'oneiric'],
+#    'diamondback': ['lucid', 'maverick', 'natty'],
+#    'cturtle': ['lucid', 'maverick', 'karmic'],
 #    'boxturtle': ['hardy', 'intrepid', 'jaunty', 'karmic'],    
 }
 
 os_test_platform = {
     'testing': ['lucid', 'maverick'],
     'unstable': ['lucid', 'oneiric'],
+    'hydro': ['precise', 'quantal', 'raring'],
     'groovy': ['oneiric', 'precise'],
     'fuerte': ['lucid', 'oneiric', 'precise'],
-    'electric': ['lucid', 'natty', 'oneiric'],
-    'diamondback': ['lucid', 'natty'],
-    'cturtle': ['lucid', 'maverick', 'karmic'],
+#    'electric': ['lucid', 'natty', 'oneiric'],
+#    'diamondback': ['lucid', 'natty'],
+#    'cturtle': ['lucid', 'maverick', 'karmic'],
 }
