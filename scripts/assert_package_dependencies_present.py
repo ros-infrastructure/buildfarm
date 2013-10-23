f8d1f16d4817090efea92d424ff9c63c551a8e75
Tully Foote <tfoote@willowgarage.com>
2013-10-23 10:55:42 -0700

    pep8 cleanup

--- scripts/assert_package_dependencies_present.py
+++ scripts/assert_package_dependencies_present.py
@@ -11,15 +11,17 @@ import yaml
 
 
 def parse_options():
-    parser = argparse.ArgumentParser(description="Return 0 if all packages are found in the repository, else print missing packages and return 1.")
+    parser = argparse.ArgumentParser(
+        description="Return 0 if all packages are found in"
+        " the repository, else print missing packages and return 1.")
     parser.add_argument(dest="rootdir",
                         help='The directory for apt to use as a rootdir')
     parser.add_argument(dest="aptconffile",
                         help='The aptconffile which points to the rootdir')
     parser.add_argument(dest="packages", nargs='+',
                         help="what packages to test for.")
-    parser.add_argument("-u", "--update", dest="update", action='store_true', default=False,
-                        help="update the cache from the server")
+    parser.add_argument("-u", "--update", dest="update", action='store_true',
+                        default=False, help="update the cache from the server")
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -32,7 +34,8 @@ if __name__ == "__main__":
     if args.update:
         c.update()
 
-    c.open()  # required to recall open after updating or you will query the old data
+    # required to recall open after updating or you will query the old data
+    c.open()
 
     missing = []
     for p in args.packages:
@@ -58,7 +61,8 @@ if __name__ == "__main__":
                 for dep in [dep.strip() for dep in build_depends]:
                     dep_name_only = dep.split()[0]
                     if dep_name_only not in c:
-                        print "package %s does not have dependency [%s]" % (p, dep_name_only)
+                        print "package %s does not have dependency [%s]" % \
+                            (p, dep_name_only)
                         missing.append(dep_name_only)
 
         except Exception as ex:
